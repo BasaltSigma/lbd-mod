cd2823479003cb481fc0b1d9845172d80de19c63
arbiter91268 <kris91268@gmail.com>
2017-09-12 13:42:31 +1200

    More experimentation and exploring stuff

--- src/main/java/net/kris91268/lbd/block/BlockLightBridge.java
+++ src/main/java/net/kris91268/lbd/block/BlockLightBridge.java
@@ -1,6 +1,7 @@
 package net.kris91268.lbd.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockCauldron;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -13,10 +14,20 @@ import javax.annotation.Nullable;
 public class BlockLightBridge extends Block {
 
     public BlockLightBridge() {
-        super(Material.IRON);
+        super(Material.IRON, MapColor.STONE);
         setHardness(2.0f);
         setCreativeTab(CreativeTabs.REDSTONE);
-        setUnlocalizedName("light_bridge");
+        setUnlocalizedName("light_bridge_source");
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
     }
 
     @Nullable
